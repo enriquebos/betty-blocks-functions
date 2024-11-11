7880038dd5db0c669f9c7fede4faa1bf44c3bad1
Enrique Bos <enriquebos7090@gmail.com>
2024-11-11 20:28:45 +0100

    Inital commit

--- functions/random-number/1.0/index.js
+++ functions/random-number/1.0/index.js
@@ -1,10 +1,9 @@
-const Generate_random_number = async ({ min, max }) => {
-  const mini = Math.ceil(min);
-  const maxi = Math.floor(max);
-  const randomNumber = Math.floor(Math.random() * (maxi - mini + 1)) + mini;
-  return {
-    returning: randomNumber,
-  };
+const generateRandomNumber = async ({ min, max }) => {
+    const mini = Math.ceil(min);
+    const maxi = Math.floor(max);
+    const randomNumber = Math.floor(Math.random() * (maxi - mini + 1)) + mini;
+    return {
+        returning: randomNumber,
+    };
 };
-
-export default Generate_random_number;
+export default generateRandomNumber;
---
+++ src/random-number/1.0/index.ts
@@ -0,0 +1,19 @@
+interface RandomNumberParams {
+  min: number;
+  max: number;
+}
+
+interface RandomNumberResult {
+  returning: number;
+}
+
+const generateRandomNumber = async ({ min, max }: RandomNumberParams): Promise<RandomNumberResult> => {
+  const mini: number = Math.ceil(min);
+  const maxi: number = Math.floor(max);
+  const randomNumber: number = Math.floor(Math.random() * (maxi - mini + 1)) + mini;
+  return {
+    returning: randomNumber,
+  };
+};
+
+export default generateRandomNumber;
