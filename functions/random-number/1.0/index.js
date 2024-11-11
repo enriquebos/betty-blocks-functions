b9910091b27de0e63606646af2f9c3edde78269b
Enrique Bos <enriquebos7090@gmail.com>
2024-11-11 21:29:24 +0100

    Removed functions folder

--- functions/random-number/1.0/index.js
+++
@@ -1,9 +0,0 @@
-const generateRandomNumber = async ({ min, max, }) => {
-    const mini = Math.ceil(min);
-    const maxi = Math.floor(max);
-    const randomNumber = Math.floor(Math.random() * (maxi - mini + 1)) + mini;
-    return {
-        returning: randomNumber,
-    };
-};
-export default generateRandomNumber;
