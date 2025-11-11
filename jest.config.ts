61831ba768a4a5d99a94ad5b66ef3ffd22575e01
Enrique Bos <enriquebos7090@gmail.com>
2025-11-11 01:23:33 +0100

    fix: slow jest test speed

--- jest.config.ts
+++ jest.config.ts
@@ -2,17 +2,25 @@ import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
   testEnvironment: "node",
-  fakeTimers: { enableGlobally: true },
   transform: {
-    "^.+.tsx?$": ["ts-jest", {}],
+    "^.+.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.json" }],
   },
-  collectCoverage: true,
-  collectCoverageFrom: ["<rootDir>/src/**/**/*.{ts,tsx}", "!<rootDir>/src/utils/"],
+  collectCoverage: process.env.CI === "true",
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.{ts,tsx}",
+    "!<rootDir>/src/**/*.d.ts",
+    "!<rootDir>/src/**/function.json",
+  ],
+  fakeTimers: {
+    enableGlobally: true,
+  },
+  coverageProvider: "v8",
   coveragePathIgnorePatterns: [
     "/test-action/1.0/",
     "/background-action/1.0/",
     "/utils/templating/templayed.ts",
   ],
+  testPathIgnorePatterns: ["/node_modules/", "/functions/"],
   coverageThreshold: {
     global: {
       branches: 95,
